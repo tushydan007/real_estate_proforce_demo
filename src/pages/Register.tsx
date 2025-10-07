b5e25b8f2e5eb5dd1fbad9ec08bec46887d2c0e6
Cornelius Ezeh <cornzeh@gmail.com>
2025-10-07 18:01:19 +0100

    enhanced code

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -65,7 +65,9 @@ export default function Register() {
   return (
     <div className="flex items-center justify-center min-h-screen px-4 bg-black">
       <div className="w-full max-w-2xl p-12 rounded-2xl shadow-lg bg-[#0C111C] text-gray-200">
-        <h2 className="text-2xl font-bold text-center mb-6">Create Account</h2>
+        <h2 className="text-2xl font-bold text-center mb-6">
+          User Registration
+        </h2>
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
           {/* First Name */}
           <div>
